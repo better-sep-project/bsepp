d4e3fc07a7c39f58df7830bcc8f940ec8c2e489e
cy <40525455+rsclip@users.noreply.github.com>
2024-04-16 14:46:10 +0100

    Formatting

--- model/entriesModel.js
+++ model/entriesModel.js
@@ -1,50 +1,57 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const tocSubItemSchema = new Schema({
+const TocSubItemSchema = new Schema({
   subTitle: { type: String, required: false },
-  href: { type: String, required: false }
+  href: { type: String, required: false },
 });
 
-const tocItemSchema = new Schema({
+const TocItemSchema = new Schema({
   title: { type: String, required: true },
   href: { type: String, required: true },
-  subItems: [tocSubItemSchema]
+  subItems: [TocSubItemSchema],
 });
 
-const otherInternetResourcesSchema = new Schema({
+const OtherInternetResourcesSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
-  url: { type: String, required: true }
+  url: { type: String, required: true },
 });
 
-const relatedEntrySchema = new Schema({
+const RelatedEntrySchema = new Schema({
   relId: { type: String, required: false },
   relatedEntryTitle: { type: String, required: false },
-  relEntryUrl: { type: String, required: false }
+  relEntryUrl: { type: String, required: false },
 });
 
-const entrySchema = new Schema({
+const EntrySchema = new Schema({
   title: { type: String, required: true },
   authors: [{ type: Object, required: true }],
   dates: {
     firstPublished: { type: Date, required: false },
-    lastUpdated: { type: Date, required: false }
+    lastUpdated: { type: Date, required: false },
   },
   preamble: { type: String, required: false },
   bibliography: { type: String, required: false },
-  toc: [tocItemSchema],
-  otherInternetResources: [otherInternetResourcesSchema],
-  relatedEntries: [relatedEntrySchema],
+  toc: [TocItemSchema],
+  otherInternetResources: [OtherInternetResourcesSchema],
+  relatedEntries: [RelatedEntrySchema],
   meta: {
     scrapedAt: { type: Date, required: true },
-    sourceUrl: { type: String, required: true }
-  }
+    sourceUrl: { type: String, required: true },
+  },
 });
 
-const Entry = mongoose.model('Entry', entrySchema);
+const EntryModel = mongoose.model("EntryModel", EntrySchema);
+const TocItemModel = mongoose.model("TocItemModel", TocItemSchema);
+const TocSubItemModel = mongoose.model("TocSubItemModel", TocSubItemSchema);
+const OtherInternetResourcesModel = mongoose.model("OtherInternetResourcesModel", OtherInternetResourcesSchema);
+const RelatedEntryModel = mongoose.model("RelatedEntryModel", RelatedEntrySchema);
 
 module.exports = {
-  Entry
+  EntryModel,
+  TocItemModel,
+  TocSubItemModel,
+  OtherInternetResourcesModel,
+  RelatedEntryModel,
 };
--- scrapeEntriesContent.js
+++ scrapeEntriesContent.js
@@ -18,61 +18,60 @@ async function fetchHTML(url) {
     return null;
   }
 }
-  
+
 /**
  * Scrapes the philosophical entry from the given URL.
  * @param {string} url - The URL of the philosophical entry to scrape.
  * @returns {Promise<Object|null>} An object representing the scraped article, or null if no content could be fetched.
  */
-  async function scrapeEntry(url) {
-    const html = await fetchHTML(url);
-    if (!html) return null;
-    const $ = cheerio.load(html);
-  
-    let identifier = url.split('/entries/')[1];
-    identifier = identifier.endsWith('/') ? identifier.slice(0, -1) : identifier;
-  
-    const title = $("meta[property='citation_title']").attr("content").trim();
-  
-    let sections = [];
-    let currentSection = { children: [] };
-  
-    $('#main-text > *').each(function() {
-      const nodeName = $(this).prop("nodeName").toLowerCase();
-  
-      if (nodeName === 'h2' || nodeName === 'h3') {
-        if (currentSection && currentSection.children.length > 0) {
-          sections.push(currentSection);
-        }
-        currentSection = {
-          identifier: $(this).find('a').attr('name'),
-          title: $(this).text().trim(),
-          contentType: nodeName === 'h2' ? 'heading' : 'subheading',
-          children: []
-        };
-      } else if (nodeName === 'p' || nodeName === 'blockquote') {
-        const contentType = nodeName === 'p' ? 'text' : 'blockQuote';
-        currentSection.children.push({
-          contentType: contentType,
-          value: $(this).html().trim() // .html preserves the formatting
-        });
+async function scrapeEntry(url) {
+  const html = await fetchHTML(url);
+  if (!html) return null;
+  const $ = cheerio.load(html);
+
+  let identifier = url.split("/entries/")[1];
+  identifier = identifier.endsWith("/") ? identifier.slice(0, -1) : identifier;
+
+  const title = $("meta[property='citation_title']").attr("content").trim();
+
+  let sections = [];
+  let currentSection = { children: [] };
+
+  $("#main-text > *").each(function () {
+    const nodeName = $(this).prop("nodeName").toLowerCase();
+
+    if (nodeName === "h2" || nodeName === "h3") {
+      if (currentSection && currentSection.children.length > 0) {
+        sections.push(currentSection);
       }
-    });
-  
-    if (currentSection && currentSection.children.length > 0) {
-      sections.push(currentSection);
+      currentSection = {
+        identifier: $(this).find("a").attr("name"),
+        title: $(this).text().trim(),
+        contentType: nodeName === "h2" ? "heading" : "subheading",
+        children: [],
+      };
+    } else if (nodeName === "p" || nodeName === "blockquote") {
+      const contentType = nodeName === "p" ? "text" : "blockQuote";
+      currentSection.children.push({
+        contentType: contentType,
+        value: $(this).html().trim(), // .html preserves the formatting
+      });
     }
-  
-    const entryArticle = {
-      identifier: identifier,
-      title: title,
-      articleContent: sections
-    };
-  
-    return entryArticle;
+  });
+
+  if (currentSection && currentSection.children.length > 0) {
+    sections.push(currentSection);
   }
-  
-  
+
+  const entryArticle = {
+    identifier: identifier,
+    title: title,
+    articleContent: sections,
+  };
+
+  return entryArticle;
+}
+
 /**
  * Determines whether a link should be excluded from the scraping process based on its parent list item's text.
  * @param {CheerioStatic} $ - The Cheerio instance to use for DOM manipulation.
@@ -80,13 +79,18 @@ async function fetchHTML(url) {
  * @returns {boolean} True if the entry should be excluded, otherwise false.
  */
 function shouldExcludeEntry($, link) {
-    // Check if the immediate text of the li (excluding child elements) contains "see"
-    const liText = $(link).parent().contents().filter(function() {
-        return this.type === 'text';
-    }).text().toLowerCase();
+  // Check if the immediate text of the li (excluding child elements) contains "see"
+  const liText = $(link)
+    .parent()
+    .contents()
+    .filter(function () {
+      return this.type === "text";
+    })
+    .text()
+    .toLowerCase();
 
-    // True if "see" is found in the direct text content of the list item
-    return liText.includes("see");
+  // True if "see" is found in the direct text content of the list item
+  return liText.includes("see");
 }
 
 /**
@@ -94,35 +98,36 @@ function shouldExcludeEntry($, link) {
  * @returns {Promise<void>} A promise that resolves when all entries have been scraped.
  */
 async function scrapePlatoStanford() {
-    const indexHtml = await fetchHTML(baseURL + "/contents.html");
-    const $ = cheerio.load(indexHtml);
-    const links = $('li a[href*="entries/"]').toArray();
+  const indexHtml = await fetchHTML(baseURL + "/contents.html");
+  const $ = cheerio.load(indexHtml);
+  const links = $('li a[href*="entries/"]').toArray();
 
-    // Filter out entries to be excluded and count the ones to be processed
-    const validLinks = links.filter(link => !shouldExcludeEntry($, link));
-    console.log(`Total entries to scrape: ${validLinks.length}`);
-    
-    const bar = new ProgressBar(":bar :current/:total (:percent) :etas", {
-        total: validLinks.length,
-    });
+  // Filter out entries to be excluded and count the ones to be processed
+  const validLinks = links.filter((link) => !shouldExcludeEntry($, link));
+  console.log(`Total entries to scrape: ${validLinks.length}`);
 
-    const scrapedEntries = [];
-    for (const link of validLinks) {
-        const href = $(link).attr("href");
-        const entryUrl = `${baseURL}/${href}`;
-        const article = await scrapeEntry(entryUrl);
-        if (article) {
-            scrapedEntries.push({
-                article
-            });
-            bar.tick();
-        }
-        fs.writeFileSync("scrapedEntries.json", JSON.stringify(scrapedEntries, null, 2));
+  const bar = new ProgressBar(":bar :current/:total (:percent) :etas", {
+    total: validLinks.length,
+  });
+
+  const scrapedEntries = [];
+  for (const link of validLinks) {
+    const href = $(link).attr("href");
+    const entryUrl = `${baseURL}/${href}`;
+    const article = await scrapeEntry(entryUrl);
+    if (article) {
+      scrapedEntries.push({
+        article,
+      });
+      bar.tick();
     }
+    fs.writeFileSync(
+      "scrapedEntries.json",
+      JSON.stringify(scrapedEntries, null, 2)
+    );
+  }
 
-    console.log("All entries have been saved to scrapedEntries.json");
+  console.log("All entries have been saved to scrapedEntries.json");
 }
 
-
 scrapePlatoStanford();
-
